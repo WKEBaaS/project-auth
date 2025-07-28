c486249b0bee7e42ba8f36e0bf147b7de70f932b
youmin1017 <youmin.main@gmail.com>
2025-07-28 18:00:50 +0800

    fix: github provider enable logic

--- src/auth/providers.ts
+++ src/auth/providers.ts
@@ -14,15 +14,16 @@ export const genSocialProviders = () => {
 	}
 
 	if (Bun.env.GITHUB_ENABLED == 'true' || Bun.env.GITHUB_ENABLED == '1') {
-		console.info('GitHub social provider is enabled');
-		socialProviders.github = {
-			clientId: Bun.env.GITHUB_CLIENT_ID,
-			clientSecret: Bun.env.GITHUB_CLIENT_SECRET,
-		};
-	} else {
-		console.warn('GitHub social provider is enabled but missing credentials');
+		if (Bun.env.GITHUB_CLIENT_ID && Bun.env.GITHUB_CLIENT_SECRET) {
+			console.info('GitHub social provider is enabled');
+			socialProviders.github = {
+				clientId: Bun.env.GITHUB_CLIENT_ID,
+				clientSecret: Bun.env.GITHUB_CLIENT_SECRET,
+			};
+		} else {
+			console.warn('GitHub social provider is enabled but missing credentials');
+		}
 	}
-
 	if (Bun.env.DISCORD_ENABLED == 'true' || Bun.env.DISCORD_ENABLED == '1') {
 		if (Bun.env.DISCORD_CLIENT_ID && Bun.env.DISCORD_CLIENT_SECRET) {
 			console.info('Discord social provider is enabled');
