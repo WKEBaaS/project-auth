c25a0ce5efc0c946a7adf795506185edc67508c3
youmin1017 <youmin.main@gmail.com>
2025-10-05 13:35:11 +0800

    draft: fix cookie

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,27 +1,28 @@
-declare module "bun" {
-  interface Env {
-    BETTER_AUTH_URL: string; // Base URL of your app
-    BETTER_AUTH_SECRET: string; // Secret key for signing tokens
-    DATABASE_URL: string; // Database connection string
-    // Optional Configurations
-    COOKIE_SECURE?: string; // Set to 'true' if using HTTPS
-    COOKIE_SAME_SITE?: string; // 'Lax', 'Strict', or 'None'
-    // Logger
-    LOGGER_ENABLED?: string; // Enable or disable logging
-    LOGGER_LEVEL?: string; // Set logging level: 'error', 'warn', 'info', 'debug'
-    // Social Providers
-    PORT?: string; // Port to run the server on
-    JWT_ENABLED?: string; // Enable JWT authentication
-    EMAIL_AND_PASSWORD_ENABLED?: string; // Enable email/password authentication
-    TRUSTED_ORIGINS?: string; // Comma-separated list of trusted origins for CORS
-    GOOGLE_ENABLED?: string; // Enable Google authentication
-    GOOGLE_CLIENT_ID?: string;
-    GOOGLE_CLIENT_SECRET?: string;
-    GIRHUB_ENABLED?: string; // Enable Facebook authentication
-    GITHUB_CLIENT_ID?: string;
-    GITHUB_CLIENT_SECRET?: string;
-    DISCORD_ENABLED?: string; // Enable Discord authentication
-    DISCORD_CLIENT_ID?: string;
-    DISCORD_CLIENT_SECRET?: string;
-  }
+declare module 'bun' {
+	interface Env {
+		BETTER_AUTH_URL: string; // Base URL of your app
+		BETTER_AUTH_SECRET: string; // Secret key for signing tokens
+		DATABASE_URL: string; // Database connection string
+		// Optional Configurations
+		COOKIE_SECURE?: string; // Set to 'true' if using HTTPS
+		COOKIE_SAME_SITE?: string; // 'Lax', 'Strict', or 'None'
+		COOKIE_DOMAIN?: string; // Domain for the cookie
+		// Logger
+		LOGGER_ENABLED?: string; // Enable or disable logging
+		LOGGER_LEVEL?: string; // Set logging level: 'error', 'warn', 'info', 'debug'
+		// Social Providers
+		PORT?: string; // Port to run the server on
+		JWT_ENABLED?: string; // Enable JWT authentication
+		EMAIL_AND_PASSWORD_ENABLED?: string; // Enable email/password authentication
+		TRUSTED_ORIGINS?: string; // Comma-separated list of trusted origins for CORS
+		GOOGLE_ENABLED?: string; // Enable Google authentication
+		GOOGLE_CLIENT_ID?: string;
+		GOOGLE_CLIENT_SECRET?: string;
+		GIRHUB_ENABLED?: string; // Enable Facebook authentication
+		GITHUB_CLIENT_ID?: string;
+		GITHUB_CLIENT_SECRET?: string;
+		DISCORD_ENABLED?: string; // Enable Discord authentication
+		DISCORD_CLIENT_ID?: string;
+		DISCORD_CLIENT_SECRET?: string;
+	}
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,48 +1,46 @@
-import { isEnabled } from "@/utils";
+import { isEnabled } from '@/utils';
 
-const trustedOrigins =
-  Bun.env.TRUSTED_ORIGINS && Bun.env.TRUSTED_ORIGINS.length > 0
-    ? Bun.env.TRUSTED_ORIGINS.split(",")
-    : undefined;
+const trustedOrigins = Bun.env.TRUSTED_ORIGINS && Bun.env.TRUSTED_ORIGINS.length > 0
+	? Bun.env.TRUSTED_ORIGINS.split(',')
+	: undefined;
 
 export const config = {
-  trustedOrigins: trustedOrigins,
-  enailAndPassword: {
-    enabled: isEnabled(Bun.env.EMAIL_AND_PASSWORD_ENABLED),
-  },
-  jwt: {
-    enabled: isEnabled(Bun.env.JWT_ENABLED),
-  },
-  cookie: {
-    secure: isEnabled(Bun.env.COOKIE_SECURE),
-    sameSite: (Bun.env.COOKIE_SAME_SITE as "Lax" | "Strict" | "None") || "Lax",
-    prefix: (Bun.env.COOKIE_PREFIX as "host" | "secure" | undefined) ||
-      undefined,
-  },
-  logger: {
-    enabled: isEnabled(Bun.env.LOGGER_ENABLED),
-    level: (Bun.env.LOGGER_LEVEL as "error" | "warn" | "info" | "debug") ||
-      "info",
-  },
-  providers: {
-    google: {
-      enabled: isEnabled(Bun.env.GOOGLE_ENABLED),
-      clientId: Bun.env.GOOGLE_CLIENT_ID,
-      clientSecret: Bun.env.GOOGLE_CLIENT_SECRET,
-    },
-    github: {
-      enabled: isEnabled(Bun.env.GITHUB_ENABLED),
-      clientId: Bun.env.GITHUB_CLIENT_ID,
-      clientSecret: Bun.env.GITHUB_CLIENT_SECRET,
-    },
-    discord: {
-      enabled: isEnabled(Bun.env.DISCORD_ENABLED),
-      clientId: Bun.env.DISCORD_CLIENT_ID,
-      clientSecret: Bun.env.DISCORD_CLIENT_SECRET,
-    },
-  },
+	trustedOrigins: trustedOrigins,
+	enailAndPassword: {
+		enabled: isEnabled(Bun.env.EMAIL_AND_PASSWORD_ENABLED),
+	},
+	jwt: {
+		enabled: isEnabled(Bun.env.JWT_ENABLED),
+	},
+	cookie: {
+		secure: isEnabled(Bun.env.COOKIE_SECURE),
+		sameSite: (Bun.env.COOKIE_SAME_SITE as 'Lax' | 'Strict' | 'None') || 'Lax',
+		domain: Bun.env.COOKIE_DOMAIN,
+	},
+	logger: {
+		enabled: isEnabled(Bun.env.LOGGER_ENABLED),
+		level: (Bun.env.LOGGER_LEVEL as 'error' | 'warn' | 'info' | 'debug') ||
+			'info',
+	},
+	providers: {
+		google: {
+			enabled: isEnabled(Bun.env.GOOGLE_ENABLED),
+			clientId: Bun.env.GOOGLE_CLIENT_ID,
+			clientSecret: Bun.env.GOOGLE_CLIENT_SECRET,
+		},
+		github: {
+			enabled: isEnabled(Bun.env.GITHUB_ENABLED),
+			clientId: Bun.env.GITHUB_CLIENT_ID,
+			clientSecret: Bun.env.GITHUB_CLIENT_SECRET,
+		},
+		discord: {
+			enabled: isEnabled(Bun.env.DISCORD_ENABLED),
+			clientId: Bun.env.DISCORD_CLIENT_ID,
+			clientSecret: Bun.env.DISCORD_CLIENT_SECRET,
+		},
+	},
 };
 
-if (config.logger.level === "debug") {
-  console.log("Config:", config);
+if (config.logger.level === 'debug') {
+	console.log('Config:', config);
 }
